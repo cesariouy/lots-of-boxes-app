8fe8f927434729a5d19df1bbbe109eeaac86731b
cesariouy <cesariog.uy@gmail.com>
2013-10-19 23:48:43 -0400

    Update BoxShowView mixin via GitHub editor

--- app/assets/javascripts/views/box_show_view_mixin.js
+++ app/assets/javascripts/views/box_show_view_mixin.js
@@ -1,3 +1,73 @@
 var BoxShowView = {
+  
+  listenOnMemberships: function() {
+    var that = this;
+    that.boxMemberships = new LotsOfBoxesApp.Collections.BoxMemberships(
+      that.model.get('box_memberships')
+    );
+    that.listenTo(that.boxMemberships, "add", function() {
+      that.render();
+    });
+    that.listenTo(that.boxMemberships, "remove", function() {
+      that.render();
+    });
+  },
+  
+  listenOnPosts: function() {
+    var that = this;
+    that.posts = new LotsOfBoxesApp.Collections.Posts(
+      that.model.get('posts')
+    );
+    that.listenTo(that.posts, "add", function() {
+      that.model.fetch({
+        success: function() {
+          that.posts.reset(that.model.get('posts'));
+          that.render();
+        }
+      });
+    });
+    // that.listenTo(that.posts, "remove", function() {
+    //   that.render();
+    // });
+  },
+  
+  renderForm: function() {
+    var that = this;
+    that.formView = new LotsOfBoxesApp.Views.PostForm({
+      model: that.model,
+      collection: that.posts
+    });
+    $('#add-form').html(that.formView.render().el);
+  },
+  
+  events: {
+    "click button.follow": "follow",
+    "click button.unfollow": "unfollow"
+  },
+
+  findMembership: function() {
+    var that = this;
+    var boxMembership = that.boxMemberships.findWhere({
+      user_id: CURRENT_USER_ID
+    });
+
+    return boxMembership;
+  },
+  
+  constructH2: function() {
+    var that = this;
+    var $h2 = $('<h2></h2>');
+    
+    var title = that.model.escape('title');
+    var idString = that.model.get('id').toString();
+    var boxType = that.model.get('type').toLowerCase();
+    var boxNumStr = " (" + boxType + " #" + idString + ",";
+    var numPostsStr = " posts: " + that.model.get('posts').length + ") ";
+    
+    var titleContent = title + boxNumStr + numPostsStr;
+    $h2.html(titleContent);
+    
+    return $h2;
+  }
 
 };

be5efbc04c9c8379099c502cf3d4f31d287381e6
cesariouy <cesariog.uy@gmail.com>
2013-10-19 23:00:26 -0400

    Remove renderShow from BoxesIndexView mixin via GitHub editor

--- app/assets/javascripts/views/boxes_index_view_mixin.js
+++ app/assets/javascripts/views/boxes_index_view_mixin.js
@@ -12,17 +12,6 @@ var BoxesIndexView = {
     });
   },
   
-  renderShow: function(rootStr) {
-    var that = this;
-    event.preventDefault();
-    var classString = $(event.currentTarget).attr('class');
-    that.stopListening();
-
-    // first item in class box id
-    var idClass = classString.split(" ")[0];
-    Backbone.history.navigate(rootStr + idClass, {trigger: true});
-  },
-  
   constructListItem: function(box) {
     var $li = $('<li></li>');
     var idString = box.get('id').toString();
